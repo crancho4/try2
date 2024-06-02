b5da6e3c131ff94a1f588cce7c9604ce19334cfa
crancho4 <gamingpeace2007@gmail.com>
2024-06-01 19:58:48 -0700

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div style={{ fontSize: 500 }}>VALO GELINMU HACU</div>
+    <div>
+      <div style={{ fontSize: 500 }}>VALO</div>
+      <img src="https://i.scdn.co/image/ab67616100005174f777c8d6f705fa1e32f75b86" alt="" />
+    </div>
   )
 }
 
