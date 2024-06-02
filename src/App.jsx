cfabc80ada40d08b5973c2242414f4afbf1f2320
crancho4 <gamingpeace2007@gmail.com>
2024-06-02 11:09:27 -0700

    ilyaas.mp3   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(new Audio('/ilyaas.mp3'));
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  audioRef.current.loop = true;
+
   return (
     <div>
-      <video muted loop autoPlay style={{ width: 1500 }} src="iphone-15-vid.mp4" />
+      <button onClick={togglePlay} style={{ color: isPlaying ? 'red' : 'white', width: isPlaying ? 1200 : '', height: isPlaying ? 500 : '', fontSize: isPlaying ? 100 : ''  }}>{isPlaying ? 'ILYAAAAAAAASSSSS' : 'ilyas'}</button>
     </div>
-  )
+  );
+
 }
 
-export default App
+export default App;
